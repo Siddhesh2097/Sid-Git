1c0d31f811259933ab751d2b702b4b286613a2e3
Siddhesh Pujari <siddheshpujari25@gmail.com>
2020-02-04 21:50:29 +0530

    Add files via upload

--- fibrecursion.sh
+++ fibrecursion.sh
@@ -4,18 +4,19 @@ if [[ $1 -le 1 ]]
 then
     echo $1
 else
-    let a=$1-1
-    let b=$1-2
-    a1=$(fibo $a)
-    b1=$(fibo $b)
-    echo "$a1 + $b1" | bc
+    #let a=$1-1
+    #let b=$1-2
+    #a1=$(fibo $a)
+    #b1=$(fibo $b)
+    #echo "$a1 + $b1" | bc
+    echo "$(($(fibo $(($1-1 | bc)))+$(fibo $(($1-2 | bc))) | bc))" 
 fi
 }
 
-read -p "enter a no" n
-for i in $(seq 0 $n)
+read -p "enter a no : " n
+for i in $(seq 0 $(($n-1 | bc)))
 do
-  echo -n "$(fibo $i) "
+  echo "$(fibo $i) "
 done
 
 
